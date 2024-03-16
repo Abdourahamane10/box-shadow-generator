7527d584f80676f0960de3deb16f0da8d82aa841
Abdourahamane <amdroudjae@gmail.com>
2024-03-16 22:34:31 +0100

    Création du state shadows

---
+++ src/features/shadows.js
@@ -0,0 +1,67 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    id: nanoid(8),
+    active: true,
+    inset: false,
+    inputs: [
+        {
+            inputNumber: 1,
+            name: "Horizontal offset",
+            value: 0,
+            type: "Range",
+            minMax: [-250, 250]
+        },
+        {
+            inputNumber: 2,
+            name: "Vertical offset",
+            value: 10,
+            type: "Range",
+            minMax: [-250, 250]
+        },
+        {
+            inputNumber: 3,
+            name: "Blur radius",
+            value: 15,
+            type: "Range",
+            minMax: [0, 250]
+        },
+        {
+            inputNumber: 4,
+            name: "Spread offset",
+            value: -3,
+            type: "Range",
+            minMax: [-250, 250]
+        },
+        {
+            inputNumber: 5,
+            name: "Color",
+            value: "#4f4f4f",
+            type: "color",
+            /* Pas de minMax vu que c'est des couleurs */
+        }
+    ]
+}
+
+export const shadowSlice = createSlice({
+    name: "shadows",
+    initialState,
+    reducers: {
+        removeShadow: (state, action) => {
+
+        },
+        addShadow: (state, action) => {
+            
+        },
+        updateShadowValue: (state, action) => {
+            
+        },
+        updateCheckbox: (state, action) => {
+            
+        }
+    }
+})
+
+export const {removeShadow, addShadow, updateShadowValue, updateCheckbox} = shadowSlice.actions
+export default shadowSlice.reducer

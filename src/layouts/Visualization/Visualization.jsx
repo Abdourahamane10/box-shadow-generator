96d331262cc3ca815b4bd9fa74c720e915d2c4bc
Abdourahamane <amdroudjae@gmail.com>
2024-03-19 17:18:53 +0100

    Mise en place du contenu du composent Visualization

--- src/layouts/Visualization/Visualization.jsx
+++ src/layouts/Visualization/Visualization.jsx
@@ -1,5 +1,12 @@
+import ModalBtn from "./Modal/ModalBtn";
+
 export default function Visualization() {
   return (
-    <div>Visualization</div>
+    <div className="flex flex-col p-5 ml-10 lg:ml-28">
+        <ModalBtn/>
+        <div className="w-[250px] h-[250px] bg-white rounded-xl mb-20 lg:mb-40">
+
+        </div>
+    </div>
   )
 }

dcd14a3c69f3f5df0ee74900920d491a7c3c580f
Abdourahamane <amdroudjae@gmail.com>
2024-03-19 16:32:06 +0100

    Mise en place du contenu du composent ShadowCheckbox

--- src/layouts/ShadowList/ShadowCheckbox.jsx
+++ src/layouts/ShadowList/ShadowCheckbox.jsx
@@ -1,5 +1,22 @@
-export default function ShadowCheckbox() {
+import { useSelector, useDispatch } from "react-redux"
+import { updateCheckbox } from "../../features/shadows"
+
+export default function ShadowCheckbox({name, shadowID}) {
+
+    const dispatch = useDispatch()
+
+    const checkboxShadow = useSelector(state => state.shadows.find(shadow => shadow.id === shadowID))
+
   return (
-    <div>ShadowCheckbox</div>
+    <>
+    <input onChange={() => dispatch(updateCheckbox({shadowID, name}))} type="checkbox"
+    checked={checkboxShadow[name]}
+    id={`checkbox-${name}-${shadowID}`}
+    className="h-4 w-4 border-gray-300 rounded mr-2"
+     />
+    <label 
+    className="leading-4 mr-5"
+    htmlFor={`checkbox-${name}-${shadowID}`}>{name.charAt(0).toUpperCase() + name.slice(1)}</label>
+    </>
   )
 }

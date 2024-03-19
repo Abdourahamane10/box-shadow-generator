b026a6c89749d70866340eec6edf97b3397c5199
Abdourahamane <amdroudjae@gmail.com>
2024-03-19 18:27:16 +0100

    Mise en place du contenu du composent ModalBtn

--- src/layouts/Visualization/Modal/ModalBtn.jsx
+++ src/layouts/Visualization/Modal/ModalBtn.jsx
@@ -1,5 +1,7 @@
 export default function ModalBtn() {
   return (
-    <div>ModalBtn</div>
+    <div className="relative z-0 mx-auto mt-2 py-1 px-3 text-sm cursor-pointer rounded bg-blue-600 text-white hover:bg-blue-700">
+        Get the code
+    </div>
   )
 }

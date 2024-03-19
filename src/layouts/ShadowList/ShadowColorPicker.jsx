ac41ac53c99219546f8cee2fa180b7352a17c977
Abdourahamane <amdroudjae@gmail.com>
2024-03-19 15:07:31 +0100

    Mise en place du contenu du composent ShadowColorPicker

--- src/layouts/ShadowList/ShadowColorPicker.jsx
+++ src/layouts/ShadowList/ShadowColorPicker.jsx
@@ -1,5 +1,25 @@
-export default function ShadowColorPicker() {
+import { useDispatch } from "react-redux"
+import { updateShadowValue } from "../../features/shadows"
+
+export default function ShadowColorPicker({inputData, shadowID}) {
+
+  const dispatch = useDispatch()
+
+  function handleInputs(e){
+    dispatch(updateShadowValue({
+      inputNumber: inputData.inputNumber,
+      value: e.target.value,
+      shadowID
+    }))
+  }
+
   return (
-    <div>ShadowColorPicker</div>
+    <div className="mt-3">
+      <p>{inputData.name}</p>
+      <div className="flex mt-2">
+        <input onChange={handleInputs} value={inputData.value} className="flex-grow border py-2 px-3 focus:outline-1 outline-gray-400" type="text" />
+        <input onChange={handleInputs} value={inputData.value} className="h-[40px] cursor-pointer" type="color" />
+      </div>
+    </div>
   )
 }

3f0181d544fc13136c132d9287e18ede68d48df5
Abdourahamane <amdroudjae@gmail.com>
2024-03-20 16:44:27 +0100

    Affichage du composant ModalResult dans le composant ModalBtn

--- src/layouts/Visualization/Modal/ModalBtn.jsx
+++ src/layouts/Visualization/Modal/ModalBtn.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react"
+/* Fonction qui permet d'ajouter un elt dans une partie du DOM */
+import { createPortal } from "react-dom"
+import ModalResult from "./ModalResult"
+
 export default function ModalBtn() {
+  const [showModal, setShowModal] = useState(false)
+
   return (
-    <div className="relative z-0 mx-auto mt-2 py-1 px-3 text-sm cursor-pointer rounded bg-blue-600 text-white hover:bg-blue-700">
+    <>
+    <button onClick={() => setShowModal(!showModal)} className="relative z-0 mx-auto mt-2 py-1 px-3 text-sm cursor-pointer rounded bg-blue-600 text-white hover:bg-blue-700">
         Get the code
-    </div>
+    </button>
+    {/* lorsque showModal est true, on affiche le modall */}
+    {showModal && createPortal(<ModalResult closeModal={() => setShowModal(!showModal)} />, document.body)}
+    </>
   )
 }

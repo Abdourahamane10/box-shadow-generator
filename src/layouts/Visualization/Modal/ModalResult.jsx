cdad7b3d788ee1a16a5aab0b8a74da86b257cf98
Abdourahamane <amdroudjae@gmail.com>
2024-03-20 22:54:46 +0100

    Mise en place du contenu du composant ModalResult

--- src/layouts/Visualization/Modal/ModalResult.jsx
+++ src/layouts/Visualization/Modal/ModalResult.jsx
@@ -1,9 +1,33 @@
 import getBoxShadowValue from "../../../utils/getBoxShadowValue"
 import { useSelector } from "react-redux"
+import { useEffect } from "react"
 
 export default function ModalResult({closeModal}) {
 
   const shadowValues = useSelector(state => state.shadows)
+
+  /* Pour gérer le scroll avec l'affichage de la modale */
+  useEffect(() => {
+    /* On empêche le scroll quand la modale est ouverte */
+    document.body.style.overflowY = "hidden"
+    /* On retourne une fonction de nettoyage qui va remmettre le scroll juste avant la disparition (destruction) de la modale. Cette fonction sera exécutée que juste avant destruction de la modale et non après 1ere création du composant et non juste avant mise à jour du composanr car on a mis un tableau en 2nd argument et il est vide */
+    document.body.style.overflowY = "auto"
+  }, [])
+
+  /* On évite  le spam (ne pas laisser cliquer plusieurs fois si l'utilisateur a déjà copié le code) pour le bouton "Copy" */
+  let runningAnimation = false
+  function handleCopy(e){
+    if(!runningAnimation){
+      runningAnimation = true
+      e.target.textContent = "Copied !"
+      setTimeout(() => {
+        e.target.textContent = "Copy"
+        runningAnimation = false
+      }, 1250)
+    }
+    /* On copie dans la presse papier le code du box-shadow */
+    navigator.clipboard.writeText(`box-shadow: ${getBoxShadowValue(shadowValues)}`)
+  }
 
   return (
     <div onClick={closeModal} className="fixed z-10 flex items-center justify-center inset-0 bg-gray-500/75">
@@ -13,7 +37,7 @@ export default function ModalResult({closeModal}) {
       className="max-w-[400px] rounded p-7 bg-gray-50 mb-[10vh]">
         <div className="flex items-end mb-5">
           <p className="font-semibold mr-5">Here is your code 🎷</p>
-          <button className="ml-auto mr-2 text-sm bg-blue-600 text-white hover:bg-blue-700 py-1 px-3 rounded">
+          <button onClick={handleCopy} className="ml-auto mr-2 text-sm bg-blue-600 text-white hover:bg-blue-700 py-1 px-3 rounded">
             Copy
           </button>
           <button
